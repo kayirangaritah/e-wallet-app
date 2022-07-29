8dd1fb5d94537e34396728e347dda9e8962edd0c
kayirangaritah <110123099+kayirangaritah@users.noreply.github.com>
2022-07-29 09:50:08 +0200

    Add files via upload

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,28 +11,93 @@ const amount = document.getElementById("amount");
 const pin = document.getElementById("pin");
 const balanceHolder = document.getElementById("balance");
 
+let initAmount;
+let recAmount;
+
 emailHolder.innerHTML = email;
 
 db.collection("balance").doc(email)
     .onSnapshot((doc) => {
         balanceHolder.innerHTML = doc.data().amount;
+        initAmount = doc.data().amount;
     });
 
+let users = [];
 
-sendBtn.addEventListener("click", (e)=> {
+db.collection('users').get().then(res => {
+    res.docs.forEach(doc => {
+        users.push(doc.data());
+    })
+})
+
+const fb = document.getElementById("feedback");
+
+
+sendBtn.addEventListener("click", async(e)=> {
     e.preventDefault();
-
-    db.collection("transaction").add({
-        senderEmail: email,
-        recipientEmail: recEmail.value,
-        amount: amount.ariaValueMax,
-        transaction: "Send",
-        createdAt: new Date()
-    })
-    .then(res => {
-        alert(`${amount.value} RWF sent to ${email.value}successfully.`)
-    })
-    .catch(err=> {
-        alert(err.message)
+    await db.collection("balance").doc(recEmail.value)
+        .onSnapshot((doc) => {
+            recAmount = doc.data().amount;
+        });
+    users.forEach(user => {
+        if(user.email === recEmail.value){
+            fb.innerHTML = "";
+            if(amount.value >= initAmount){
+                alert("Amount is bigger than your current balance!");
+            }else{
+                db.collection("transaction").add({
+                    senderEmail: email,
+                    recipientEmail: recEmail.value,
+                    amount: amount.value,
+                    transaction: "Send",
+                    createdAt: Date.now()
+                })
+                .then(res => {
+                    switch(amount.value){
+                        case amount.value < 0:
+                            fb.innerHTML = "invalid amount";
+                            break;
+                        case amount.value > 0 && amount.value <= 10000:
+                            db.collection("balance").doc(email).update({
+                                amount: parseInt(initAmount) - parseInt(amount.value),
+                                lastUpdated: new Date()
+                            })
+                            break;
+                        case amount.value > 10000 && amount.value <= 100000:
+                            db.collection("balance").doc(email).update({
+                                amount: parseInt(initAmount) - parseInt(amount.value) - 200,
+                                lastUpdated: new Date()
+                            })
+                            break;
+                        case amount.value > 100000:
+                            db.collection("balance").doc(email).update({
+                                amount: parseInt(initAmount) - parseInt(amount.value) - 1000,
+                                lastUpdated: new Date()
+                            })
+                            break;
+                            
+                    }
+                    alert(`${amount.value} RWF sent to ${recEmail.value} successfully.`)
+                    db.collection("transaction").add({
+                        senderEmail: email,
+                        recipientEmail: recEmail.value,
+                        amount: amount.value,
+                        transaction: "Receive",
+                        createdAt: Date.now()
+                    }).then(res=> {
+                        db.collection("balance").doc(recEmail.value).update({
+                            amount: parseInt(recAmount) + parseInt(amount.value),
+                            lastUpdated: new Date()
+                        })
+                    })
+                })
+                .catch(err=> {
+                    alert(err.message)
+                })
+            }
+            return true;
+        }else{
+           return false;
+        }
     })
 })
